157ab55e5260896684353b66d706ee4364cac4a0
BA2335 <BA2335@BR3PC08>
2023-02-15 15:09:32 +0000

    Adicionar arquivos de projeto.

---
+++ RVmIVn/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace RVmIVn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rdbRomanNumeral = new System.Windows.Forms.RadioButton();
+            this.rdbDecimal = new System.Windows.Forms.RadioButton();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rdbRomanNumeral
+            // 
+            resources.ApplyResources(this.rdbRomanNumeral, "rdbRomanNumeral");
+            this.rdbRomanNumeral.Checked = true;
+            this.rdbRomanNumeral.Name = "rdbRomanNumeral";
+            this.rdbRomanNumeral.TabStop = true;
+            this.rdbRomanNumeral.UseVisualStyleBackColor = true;
+            this.rdbRomanNumeral.CheckedChanged += new System.EventHandler(this.rdbRomanNumeral_CheckedChanged);
+            // 
+            // rdbDecimal
+            // 
+            resources.ApplyResources(this.rdbDecimal, "rdbDecimal");
+            this.rdbDecimal.Name = "rdbDecimal";
+            this.rdbDecimal.UseVisualStyleBackColor = true;
+            this.rdbDecimal.CheckedChanged += new System.EventHandler(this.rdbDecimal_CheckedChanged);
+            // 
+            // txtOutput
+            // 
+            resources.ApplyResources(this.txtOutput, "txtOutput");
+            this.txtOutput.Name = "txtOutput";
+            // 
+            // txtInput
+            // 
+            resources.ApplyResources(this.txtInput, "txtInput");
+            this.txtInput.Name = "txtInput";
+            // 
+            // btnConvert
+            // 
+            resources.ApplyResources(this.btnConvert, "btnConvert");
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // btnClear
+            // 
+            resources.ApplyResources(this.btnClear, "btnClear");
+            this.btnClear.Name = "btnClear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnHelp
+            // 
+            resources.ApplyResources(this.btnHelp, "btnHelp");
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // btnCopy
+            // 
+            resources.ApplyResources(this.btnCopy, "btnCopy");
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnPaste
+            // 
+            resources.ApplyResources(this.btnPaste, "btnPaste");
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.rdbDecimal);
+            this.Controls.Add(this.rdbRomanNumeral);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdbRomanNumeral;
+        private System.Windows.Forms.RadioButton rdbDecimal;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnPaste;
+    }
+}
+
---
+++ RVmIVn/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RVmIVn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (rdbRomanNumeral.Checked)
+            {
+                int decimalNumber;
+                if (int.TryParse(txtInput.Text, out decimalNumber))
+                {
+                    string romanNumeral = ConvertToRomanNumeral(decimalNumber);
+                    txtOutput.Text = romanNumeral;
+                }
+                else
+                {
+                    MessageBox.Show("Input Invalido. Tenta colocar um Número Decimal.", "Erro", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+            }
+            else if (rdbDecimal.Checked)
+            {
+                string romanNumeral = txtInput.Text.ToUpper();
+                int decimalNumber = ConvertToDecimal(romanNumeral);
+                if (decimalNumber != -1)
+                {
+                    txtOutput.Text = decimalNumber.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Input Invalido. Tenta colocar um Número Romano.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private static string ConvertToRomanNumeral(int decimalNumber)
+        {
+            StringBuilder romanNumeralBuilder = new StringBuilder();
+
+            int[] values = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            string[] numerals = new string[] { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                while (decimalNumber >= values[i])
+                {
+                    decimalNumber -= values[i];
+                    romanNumeralBuilder.Append(numerals[i]);
+                }
+            }
+
+            return romanNumeralBuilder.ToString();
+        }
+
+        private static int ConvertToDecimal(string romanNumeral)
+        {
+            Dictionary<char, int> romanValues = new Dictionary<char, int>()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+            int result = 0;
+            int prevValue = 0;
+
+            foreach (char c in romanNumeral)
+            {
+                int currentValue;
+                if (!romanValues.TryGetValue(c, out currentValue))
+                {
+                    return -1;
+                }
+
+                result += currentValue;
+
+                if (prevValue < currentValue)
+                {
+                    result -= 2 * prevValue;
+                }
+
+                prevValue = currentValue;
+            }
+
+            return result;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+            txtOutput.Text = "";
+        }
+
+        
+
+        private void rdbRomanNumeral_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdbDecimal_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (txtOutput.Text == "")
+            {
+                MessageBox.Show("Para copiar o Output, é preciso haver Output." +
+                    "\n\nPeço-te se não for muito maçante, escreve alguma coisa no Input.", "Sem Output", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Clipboard.SetText(txtOutput.Text);
+                MessageBox.Show("Copiado com sucesso!", "Copiado!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = Clipboard.GetText();
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            string message = "Se for sobre alguns caracteres que levam acentuação, alguns caracteres não estão disponiveis devido de não haver uma tradução certa para o caracter." +
+               "\n\nSe não for sobre isso, sê livre de ir ao meu Website para relatar sobre algum problema.";
+            string ws = Environment.NewLine + "\nhttps://h3nr1qu3x.github.io/website/contact.html";
+            MessageBox.Show(message + ws, "Ajuda", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
